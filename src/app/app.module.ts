71010d47036f9c8b0ddd3fc4b837c3f0f7c71bb7
David Kisari <kisari.david@gmail.com>
2019-02-18 11:38:02 +0100

    Empty components generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserlistComponent } from './admin/userlist/userlist.component';
+import { AdduserComponent } from './admin/userlist/adduser/adduser.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { MenusComponent } from './menus/menus.component';
+import { AddmenuComponent } from './menus/addmenu/addmenu.component';
+import { GroupsComponent } from './groups/groups.component';
+import { EditgroupComponent } from './groups/editgroup/editgroup.component';
+import { IngredientsComponent } from './ingredients/ingredients.component';
+import { RecipeCardComponent } from './recipes/recipe-card/recipe-card.component';
+import { CreateIngredientComponent } from './ingredients/create-ingredient/create-ingredient.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    LoginComponent,
+    DashboardComponent,
+    UserlistComponent,
+    AdduserComponent,
+    RegistrationComponent,
+    RecipesComponent,
+    MenusComponent,
+    AddmenuComponent,
+    GroupsComponent,
+    EditgroupComponent,
+    IngredientsComponent,
+    RecipeCardComponent,
+    CreateIngredientComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/groups/editgroup/editgroup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-editgroup',
+  templateUrl: './editgroup.component.html',
+  styleUrls: ['./editgroup.component.scss']
+})
+export class EditgroupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
