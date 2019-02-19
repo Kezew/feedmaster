4c9c9d32f05c04f781b47142bc5e3aa12ab02ed1
Kovács Szonja <ksz.oni@gmail.com>
2019-02-19 14:22:58 +0100

    NgbModule & FormsModule to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -41,6 +43,8 @@ import { CreateIngredientComponent } from './ingredients/create-ingredient/creat
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
