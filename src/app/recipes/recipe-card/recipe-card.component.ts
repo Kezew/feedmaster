fd02da57c467b9a5b7667518c03d48f0f93b8244
David Kisari <kisari.david@gmail.com>
2019-02-20 22:03:19 +0100

    Recipe card progress

--- src/app/recipes/recipe-card/recipe-card.component.ts
+++ src/app/recipes/recipe-card/recipe-card.component.ts
@@ -6,25 +6,34 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./recipe-card.component.scss"]
 })
 export class RecipeCardComponent implements OnInit {
-  ingredients;
-  referencePerson;
+  data;
 
   constructor() {
-    this.ingredients = [
-      ["Zabpehely", 75],
-      ["Zabpehely", 75],
-      ["Zabpehely", 75],
-      ["Zabpehely", 75],
-      ["Zabpehely", 75],
-      ["Zabpehely", 75],
-      ["Zabpehely", 75],
-      ["Zabpehely", 75],
-      ["Zabpehely", 75],
-      ["Zabpehely", 75],
-      ["Zabpehely", 75],
-      ["Zabpehely", 75]
-    ];
-    this.referencePerson = "Puhatestű-érzékeny 1";
+    this.data = {
+      title: "Zabkása",
+      logo: "breakfast",
+      ingredients: [
+        { name: "Zabpehely", amount: 75 },
+        { name: "Zabpehely", amount: 75 },
+        { name: "Zabpehely", amount: 75 },
+        { name: "Zabpehely", amount: 75 },
+        { name: "Zabpehely", amount: 75 },
+        { name: "Zabpehely", amount: 75 },
+        { name: "Zabpehely", amount: 75 },
+        { name: "Zabpehely", amount: 75 },
+        { name: "Zabpehely", amount: 75 },
+        { name: "Zabpehely", amount: 75 }
+      ],
+      nutrition: {
+        fat: 2.78,
+        saturatedFat: 1.56,
+        protein: 4.47,
+        carbs: 8.98,
+        sugar: 2.79,
+        kcal: 70
+      },
+      referencePerson: "Puhatestű-érzékeny 1"
+    };
   }
 
   ngOnInit() {}
