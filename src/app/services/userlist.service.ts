5825bc491c22d975a2ef869b3f1909ae48d6bc9e
K Toni <antal.krevenka@gmail.com>
2019-02-25 13:24:36 +0100

    Auto stash before rebase of "origin/master"
    userlist

--- src/app/services/userlist.service.ts
+++ src/app/services/userlist.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpService } from './http.service';
+import { User } from '../interfaces/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserlistService {
 
-  constructor() { }
+  constructor(private httpService : HttpService) { }
+
+  getUsers() : Promise<User[]> {
+      return this.httpService.get('/list-users');
+  }
 }
