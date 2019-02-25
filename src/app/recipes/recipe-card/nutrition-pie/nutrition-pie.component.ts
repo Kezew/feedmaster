8ff0c1d854d4354eb3d574b31661acd3274e4bd0
David Kisari <kisari.david@gmail.com>
2019-02-25 14:26:57 +0100

    Recipe service

--- src/app/recipes/recipe-card/nutrition-pie/nutrition-pie.component.ts
+++ src/app/recipes/recipe-card/nutrition-pie/nutrition-pie.component.ts
@@ -1,45 +1,70 @@
-import { Component, OnInit } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  OnChanges,
+  Input,
+  SimpleChanges
+} from "@angular/core";
 import { Chart } from "angular-highcharts";
+
+import { NutritionData } from "src/app/interfaces/recipe";
+import { animate } from "highcharts";
 
 @Component({
   selector: "app-nutrition-pie",
   templateUrl: "./nutrition-pie.component.html",
   styleUrls: ["./nutrition-pie.component.scss"]
 })
-export class NutritionPieComponent implements OnInit {
-  chart = new Chart({
-    chart: {
-      type: "pie"
-    },
-    tooltip: {
-      pointFormat: "<b>{point.percentage:.1f}%</b>"
-    },
-    title: {
-      text: ""
-    },
-    credits: {
-      enabled: false
-    },
-    plotOptions: {
-      pie: {
-        size: 180,
-        allowPointSelect: false,
-        cursor: "pointer",
-        dataLabels: {
-          enabled: false
-        }
-      }
-    },
-    colors: ["#bc5f54", "#4bb89a", "#e4b101"],
-    series: [
-      {
-        type: "pie",
-        data: [["Zsír", 12.8], ["Fehérje", 8.5], ["Szénhidrát", 6.2]]
-      }
-    ]
-  });
+export class NutritionPieComponent implements OnInit, OnChanges {
+  @Input() data: NutritionData;
+  chart: Chart;
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.createChart(this.data);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.createChart(this.data);
+  }
+
+  createChart(data): void {
+    this.chart = new Chart({
+      chart: {
+        type: "pie"
+      },
+      tooltip: {
+        pointFormat: "<b>{point.percentage:.1f}%</b>"
+      },
+      title: {
+        text: ""
+      },
+      credits: {
+        enabled: false
+      },
+      plotOptions: {
+        pie: {
+          size: 180,
+          allowPointSelect: false,
+          cursor: "pointer",
+          dataLabels: {
+            enabled: false
+          }
+        }
+      },
+      colors: ["#bc5f54", "#4bb89a", "#e4b101"],
+      series: [
+        {
+          type: "pie",
+          data: [
+            ["Zsír", this.data.fat],
+            ["Fehérje", this.data.protein],
+            ["Szénhidrát", this.data.carbs]
+          ],
+          animation: false
+        }
+      ]
+    });
+  }
 }
--- src/app/recipes/recipe-card/recipe-card.component.ts
+++ src/app/recipes/recipe-card/recipe-card.component.ts
@@ -1,42 +1,92 @@
-import { Component, OnInit } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  OnChanges,
+  Input,
+  SimpleChanges,
+  Output
+} from "@angular/core";
+import { Recipe, Ingredient, NutritionData } from "src/app/interfaces/recipe";
+import { RecipeService } from "src/app/services/recipe.service";
 
 @Component({
   selector: "app-recipe-card",
   templateUrl: "./recipe-card.component.html",
   styleUrls: ["./recipe-card.component.scss"]
 })
-export class RecipeCardComponent implements OnInit {
-  data;
+export class RecipeCardComponent implements OnInit, OnChanges {
+  @Input() recipe: Recipe;
+  @Output() data: NutritionData;
   editMode: boolean;
+  recipeClone: Recipe;
 
-  constructor() {
+  constructor(private recipeService: RecipeService) {
+    this.resetData();
+  }
+
+  ngOnInit() {
+    this.calculateNutrition();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {}
+
+  private resetData(): void {
     this.data = {
-      title: "Zabkása",
-      ingredients: [
-        { name: "Zabpehely", amount: 75 },
-        { name: "Zabpehely", amount: 75 },
-        { name: "Zabpehely", amount: 75 },
-        { name: "Zabpehely", amount: 75 },
-        { name: "Zabpehely", amount: 75 },
-        { name: "Zabpehely", amount: 75 }
-      ],
-      nutrition: {
-        fat: 2.78,
-        saturatedFat: 1.56,
-        protein: 4.47,
-        carbs: 8.98,
-        sugar: 2.79,
-        kcal: 70
-      },
-      referencePerson: "Glutén-érzékeny 1"
+      fat: 0,
+      saturatedFat: 0,
+      protein: 0,
+      carbs: 0,
+      sugar: 0,
+      kcal: 0
     };
   }
 
-  ngOnInit() {}
+  private calculateNutrition(): void {
+    this.resetData();
+    let r: Recipe;
+    if (this.editMode) {
+      r = this.recipeClone;
+    } else {
+      r = this.recipe;
+    }
+    r.ingredients.forEach(e => {
+      this.data.fat +=
+        this.getIngredient(e.ingredientId).fat * (e.ingredientQuantity / 100);
+      this.data.saturatedFat +=
+        this.getIngredient(e.ingredientId).saturatedFat *
+        (e.ingredientQuantity / 100);
+      this.data.protein +=
+        this.getIngredient(e.ingredientId).protein *
+        (e.ingredientQuantity / 100);
+      this.data.carbs +=
+        this.getIngredient(e.ingredientId).carbohydrate *
+        (e.ingredientQuantity / 100);
+      this.data.sugar +=
+        this.getIngredient(e.ingredientId).sugar * (e.ingredientQuantity / 100);
+      this.data.kcal +=
+        this.getIngredient(e.ingredientId).energyKcal *
+        (e.ingredientQuantity / 100);
+    });
+  }
+
+  getIngredient(id): Ingredient {
+    return this.recipeService.getIngredientById(id);
+  }
 
   toggleEditMode(): void {
     this.editMode = !this.editMode;
+    if (this.editMode) {
+      this.cloneRecipe();
+    }
   }
 
-  deleteIngredient(ingredient): void {}
+  cloneRecipe(): void {
+    this.recipeClone = JSON.parse(JSON.stringify(this.recipe));
+  }
+
+  deleteIngredient(id: number): void {
+    this.recipeClone.ingredients.find(e => {
+      return e.ingredientId === id;
+    });
+  }
 }
