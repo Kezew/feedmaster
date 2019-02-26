68241894ce5bc1fa5a4cb5f2a32b1799f2d9409f
Zita Hajdu <hajduzita88@gmail.com>
2019-02-26 11:50:50 +0100

    Login service import repair

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { User } from '../interfaces/user';
 import { HttpService } from './http.service';
-import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
