f9d6ed531a2e7af47859689addc7bc7be0b77591
Zita Hajdu <hajduzita88@gmail.com>
2019-02-26 11:45:30 +0100

    Login service - regist user + getToken

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { User } from '../interfaces/user';
 import { HttpService } from './http.service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -9,20 +10,32 @@ export class LoginService {
 
   constructor(private httpService: HttpService) { }
 
-  getUserFromToken(): Promise<User> {
-    return new Promise((resolve, reject) =>{
-      this.httpService.get("/verify-registration").then(data =>{
-        resolve(this.userReg(data));
-      });
-    });
-  }
-
-  private userReg(data:any): User{
+  private userReg(data: any): User {
     return data;
   }
 
-  loginUser(data: User): Promise<void>{
+  loginUser(data: User): Promise<void> {
     return this.httpService.postFormData('/login', data);
+  }
+  
+  getUserFromToken(token: string): Promise<User> {
+    return new Promise<User>((resolve, reject) => {
+      this.httpService.get('/verify-registration/' + token)
+        .then(response => {
+          resolve((response as any).userData);
+        }).catch(reject);
+    });
+  }
+
+  registerUser(user: User) : Promise<void> {
+    let postData = {
+      emailAddress : user.email,
+      originalUsername : user.name,
+      newUsername : user.name2,
+      password: user.password,
+      confirmPassword: user.password2,
+    };
+    return this.httpService.post('/verify-registration', postData);
   }
 
 }
