cbcf64ae0f0bd518c161e50d22d58599b0521095
Tóth Márton <tothmarci25@gmail.com>
2019-02-28 11:27:01 +0100

    menu letöltése

--- src/app/services/menu.service.ts
+++ src/app/services/menu.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Menufromserver } from '../interfaces/menufromserver';
-import { Menu, MenuItem } from '../interfaces/menu';
+import { Menu, MenuItem, Day } from '../interfaces/menu';
 import { HttpService } from './http.service';
 
 @Injectable({
@@ -18,17 +18,24 @@ export class MenuService {
     });
   }
 
-  // getMenuById(id:number): Promise<Menu>{
-  //   return new Promise<Menu>((resolve, reject) => {
-  //     this.httpService.get('/menudetails?menuId='+id).then(data => {
-  //       let menu: Menu{}  = {};
-  //       menu.name= data.menuName;
-  //       menu.id = data.menuId;
-  //       for (let key in data.weekDays) {
-  //
-  //       }
-  //
-  //     }).catch(reject);
-  //   });
-  // }
+  getMenuById(id:number): Promise<Menu>{
+    return new Promise<Menu>((resolve, reject) => {
+      this.httpService.get('/menudetails?menuId='+id).then(data => {
+        let menu: Menu  = {
+          name: '',
+          id: 0,
+          items: []
+        };
+        menu.name= data.menuName;
+        menu.id = data.menuId;
+        for (let key in data.weekDays) {
+          menu.items[Day[key.toLowerCase()]] = data.weekDays[key];
+          menu.items[Day[key.toLowerCase()]].dayNumber = Day[key.toLowerCase()] +1;
+        }
+        
+        resolve(menu);
+
+      }).catch(reject);
+    });
+  }
 }
